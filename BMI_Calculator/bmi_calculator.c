4ef212f5b5048da9ceb6e4a53351f60eed2b15b0
Han <hzaw74@gmail.com>
2025-07-16 15:47:54 +0800

    Week 2

--- BMI_Calculator/bmi_calculator.c
+++ BMI_Calculator/bmi_calculator.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
 
-// int unit_convert(int x)
+float metric (int x, int y);
+float imperial (int a, int b);
+
 
 int main (void)
 {
-    int height, weight;
+    int height, weight, unit;
     float bmi;
 
-    printf("Height: ");
-    scanf("%d", &height);
+    printf("1. Metric (kg, m)\n2. Imperial (lb, inches)\nEnter 1 or 2: ");
+    scanf("%d", &unit);
 
     printf("Weight: ");
     scanf("%d", &weight);
 
-    bmi = weight / (height * height);
-    printf("Your BMI is %.2lf\n.", bmi);
+    printf("Height: ");
+    scanf("%d", &height);
+
+    if (unit == 1)
+    {
+        bmi = metric (weight, height);
+    }
+
+    else
+    {
+        bmi = imperial (weight, height);
+    }
 
     if (bmi < 18.5)
     {
-        printf("Underweight");
+        printf("Underweight\n");
     }
 
     else if (bmi < 25)
     {
-        printf("Normal weight");
+        printf("Normal weight\n");
     }
 
+    else if(bmi <= bmi)
+    {
+        printf("Overweight\n");
+    }
+    else
+    {
+        printf("Obese\n");
+    }
+}
+
+float metric (int x, int y)
+{
+    float z = x / (y * y);
+    return z;
+}
+
+float imperial (int a, int b)
+{
+    float c = 703 * (a / (b * b));
+    return c;
 }
