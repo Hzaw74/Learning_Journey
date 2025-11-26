cce5e6579d92bfb632fbd313079cc53ef1899722
Hzaw74 <hzaw74@gmail.com>
2025-11-26 14:57:37 +0800

    all files

---
+++ Multimedia_Course/Assignment/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('bookBtn');
+
+button.addEventListener('click', () => {
+  // Replace this with your booking page URL or logic
+  console.log("Redirecting to booking engine...");
+  alert("Opening Booking Page!");
+});
